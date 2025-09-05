337f03a759e627f8ab910fe6315b4ac66fc0d3db
Queenu-7 <q.uwera@alustudent.com>
2025-09-05 10:00:00 -0700

    text me

---
+++ regular_expressions/8-textme.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+log = ARGV[0]
+
+matches = log.scan(/from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+
+# matches is an array of arrays; we just need the first match
+if matches.any?
+  sender, receiver, flags = matches[0]
+  puts "#{sender},#{receiver},#{flags}"
+end
